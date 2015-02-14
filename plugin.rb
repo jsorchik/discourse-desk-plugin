cfc7166c829f45341825b9601e1e9612fd4e761b
Josh Sorchik <jsorchik@gmail.com>
2015-02-14 14:52:02 +1100

    rubocop

--- plugin.rb
+++ plugin.rb
@@ -3,21 +3,21 @@
 # version: 0.1
 # authors: jsorchik. Forked from https://github.com/shivpkumar/Zendesk-Plugin
 
-gem 'addressable', '2.3.7', { require: false }
+gem 'addressable', '2.3.7', require: false
 gem 'simple_oauth', '0.2.0'
 gem 'desk_api', '0.6.1'
 
-register_asset "javascripts/topic_controller_cont.js"
-register_asset "javascripts/topic_route_cont.js"
-register_asset "javascripts/desk_button.js"
-register_asset "stylesheets/buttons_cont.css.scss"
+register_asset 'javascripts/topic_controller_cont.js'
+register_asset 'javascripts/topic_route_cont.js'
+register_asset 'javascripts/desk_button.js'
+register_asset 'stylesheets/buttons_cont.css.scss'
 
 after_initialize do
-  load File.expand_path("../controllers/desk_controller.rb", __FILE__)
-  load File.expand_path("../lib/desk_case.rb", __FILE__)
-  load File.expand_path("../lib/new_desk_case.rb", __FILE__)
-  load File.expand_path("../lib/existing_desk_case.rb", __FILE__)
-  load File.expand_path("../lib/desk_customer.rb", __FILE__)
+  load File.expand_path('../controllers/desk_controller.rb', __FILE__)
+  load File.expand_path('../lib/desk_case.rb', __FILE__)
+  load File.expand_path('../lib/new_desk_case.rb', __FILE__)
+  load File.expand_path('../lib/existing_desk_case.rb', __FILE__)
+  load File.expand_path('../lib/desk_customer.rb', __FILE__)
 
   Discourse::Application.routes.prepend do
     post 'desk/create_case' => 'desk#create_case'
