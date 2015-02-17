ada904d27a5eb9eb541abcf9acd50390b84de735
Josh Sorchik <jsorchik@gmail.com>
2015-02-17 13:37:16 +1100

    increment version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: desk
 # about: create and view status of desk.com case within each topic
-# version: 0.1
+# version: 0.2
 # authors: jsorchik. Forked from https://github.com/shivpkumar/Zendesk-Plugin
 
 gem 'addressable', '2.3.7', require: false
