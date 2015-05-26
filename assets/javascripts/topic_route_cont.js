49655649630934a87b626d8b836055977d4c4273
Josh Sorchik <jsorchik@gmail.com>
2015-05-26 09:43:51 -0700

    fix find_case external_id

--- assets/javascripts/topic_route_cont.js
+++ assets/javascripts/topic_route_cont.js
@@ -1,7 +1,7 @@
 Discourse.TopicRoute.on("setupTopicController", function(event) {
   Discourse.ajax("/desk/find_case", {
     dataType: 'json',
-    data: { external_id: event.currentModel.slug + event.currentModel.id },
+    data: { external_id: "community-" + event.currentModel.id },
     type: 'GET'
   }).then(function (the_case) {
     if (the_case) event.controller.setProperties({ deskCase: the_case });
